7f6d16acf2960274653a6af2c370c070e2d935d4
MOSHE NNADOZIE <nnadoziemoshe@gmail.com>
2020-04-14 08:03:38 -0700

    audit style fix

--- src/estimator.js
+++ src/estimator.js
@@ -3,25 +3,26 @@ const covid19ImpactEstimator = (data) => {
    const impact = {};
    const severeImpact = {};
    let days;
-   if (data.periodType === 'days') {
+   if (data.periodType === "days") {
        days = data.timeToElapse;
-   } else if (data.periodType === 'weeks') {
+   } else if (data.periodType === "weeks") {
        days = data.timeToElapse * 7;
-   } else (data.periodType === 'months') {
-       days = data.timeToElapse * 30;
+   } else  {
+       days = data.timeToElapse * 30; 
+    };
    };
-};
+
 
    impact.currentlyInfected = data.reportedCases * 10;
    severeImpact.currentlyInfected = data.reportedCases * 50;
    const value = Math.trunc(2**(days / 3));
 
-   impact.infectionsByRequestedTime = Math.trunc{
+   impact.infectionsByRequestedTime = Math.trunc (
        impact.currentlyInfected * 2 ** value
-   };
-   severeImpact.infectionsByRequestedTime = Math.trunc{
+   );
+   severeImpact.infectionsByRequestedTime = Math.trunc (
        severeImpact.currentlyInfected * 2 ** value
-   };
+   );
 
    return {
        data: input,
