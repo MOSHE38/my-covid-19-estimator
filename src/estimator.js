3ceed648fd6d16096b351fea80ba1f4ecfc9d923
MOSHE NNADOZIE <nnadoziemoshe@gmail.com>
2020-04-14 15:41:01 -0700

    audit fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,34 +1,18 @@
 const covid19ImpactEstimator = (data) => {
 
-   const impact = {};
-   const severeImpact = {};
-   let days;
-   if (data.periodType === "days") {
-       days = data.timeToElapse;
-   } else if (data.periodType === "weeks") {
-       days = data.timeToElapse * 7;
-   } else  {
-       days = data.timeToElapse * 30; 
-    };
-    return {
-        data: input,
-        impact:{},
-        severeImpact: {}
+    const factoriser = (data) => {
+        let getFactor;
+        if (data.periodType === 'days') {
+          getFactor = Math.trunc((data.timeToElapse * 1) / 3);
+        } else if (data.periodType === 'weeks') {
+          getFactor = Math.trunc((data.timeToElapse * 7) / 3);
+        } else if (data.periodType === 'months') {
+          getFactor = Math.trunc((data.timeToElapse * 30) / 3);
+        } else {
+          getFactor = 1;
+        }
+        return getFactor;
+      };
     }; 
-   };
-
-
-   impact.currentlyInfected = data.reportedCases * 10;
-   severeImpact.currentlyInfected = data.reportedCases * 50;
-   const value = Math.trunc(2**(days / 3));
-
-   impact.infectionsByRequestedTime = Math.trunc(
-       impact.currentlyInfected * 2 ** value
-   );
-   severeImpact.infectionsByRequestedTime = Math.trunc(
-       severeImpact.currentlyInfected * 2 ** value
-   );
-
-  
 
 export default covid19ImpactEstimator;
